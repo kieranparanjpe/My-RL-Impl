fix logging, and fix broadcasting issues

src/algorithms/algorithm.py
@@ -10,14 +10,14 @@ from .policies.policy import Policy
 class Algorithm(ABC):
 
     def __init__(self, hyperparameters, policy : Policy, obs_dimension : int, action_dimension : int,
-                 is_discrete : bool = False, wandb_run : Optional[wandb.Run]=None,
+                 discrete : bool = False, wandb_run : Optional[wandb.Run]=None,
                  device : torch.device = torch.device('cpu')):
         super().__init__()
         self.hyperparameters = hyperparameters
         self.policy = policy
         self.obs_dimension = obs_dimension
         self.action_dimension = action_dimension
-        self.is_discrete = is_discrete
+        self.discrete = discrete
         self.wandb_run = wandb_run
         self.device = device
 
@@ -27,7 +27,7 @@ class Algorithm(ABC):
         pass
 
     @abstractmethod
-    def update_and_observe(self, initial_obs : torch.Tensor, next_obs : torch.Tensor, action : torch.Tensor, action_log_prob : float, reward : float, done : bool, timestep : int):
-        """Update and observe next steps based on environment's current obs after stepping. May include gradient updates, buffer updates, etc."""
+    def update_and_observe(self, initial_obs : torch.Tensor, next_obs : torch.Tensor, action : torch.Tensor, action_log_prob : float, reward : float, done : bool, timestep : int) -> bool:
+        """Update and observe next steps based on environment's current obs after stepping. May include gradient updates, buffer updates, etc. Returns True if a policy update occurred."""
         pass
 

src/algorithms/policies/policy.py
@@ -12,26 +12,23 @@ class Policy(ABC, torch.nn.Module):
     def forward(self, observation : torch.Tensor) -> torch.distributions.Distribution:
         pass
 
+    def log_probability(self, action : torch.Tensor, distribution : torch.distributions.Distribution) -> torch.Tensor:
+        # we need to sum the log probabilities together because
+        # for an n dimensional action a with independent elements, p(a) = p(a1) * p(a2) * ... * p(an) => log(p(a)) =
+        # log(p(a1)) + ... + log(p(an))
+        return distribution.log_prob(action).sum(-1)
+
     def sample(self, obs : torch.Tensor) -> tuple[torch.Tensor, torch.Tensor, torch.distributions.Distribution]:
         """Run forward pass, sample the policy to get an action. Returns the action and its log probability"""
         distribution = self.forward(obs)
-        actions = distribution.sample()
-        log_probs = distribution.log_prob(actions).sum(-1) # we need to sum the log probabilities together because
-        # for an n dimensional action a with independent elements, p(a) = p(a1) * p(a2) * ... * p(an) => log(p(a)) =
-        # log(p(a1)) + ... + log(p(an))
+        action = distribution.sample()
+        log_probability = self.log_probability(action, distribution)
 
-        return actions, log_probs, distribution
+        return action, log_probability, distribution
 
     def log_probability_of_action(self, obs : torch.Tensor, action : torch.Tensor) -> tuple[torch.Tensor, torch.distributions.Distribution]:
         """Run forward pass, find log prob of the given action"""
         distribution = self.forward(obs)
-        log_probs = distribution.log_prob(action)
+        log_probability = self.log_probability(action, distribution)
 
-        if len(log_probs.shape) > 1:
-            log_probs = log_probs.sum(-1)
-
-        # we need to sum the log probabilities together because (only if more than 1 action)
-        # for an n dimensional action a with independent elements, p(a) = p(a1) * p(a2) * ... * p(an) => log(p(a)) =
-        # log(p(a1)) + ... + log(p(an))
-
-        return log_probs, distribution
+        return log_probability, distribution

src/algorithms/ppo.py
@@ -27,8 +27,8 @@ class PPOHyperparams(Hyperparameters):
 
 class PPO(Algorithm):
     def __init__(self, hyperparameters : Hyperparameters, policy : Policy, obs_dimension : int, action_dimension :
-    int, is_discrete : bool = False, wandb_run : Optional[wandb.Run]=None, device : torch.device = torch.device('cpu')):
-        super().__init__(hyperparameters, policy, obs_dimension, action_dimension, is_discrete, wandb_run=wandb_run,
+    int, discrete : bool = False, wandb_run : Optional[wandb.Run]=None, device : torch.device = torch.device('cpu')):
+        super().__init__(hyperparameters, policy, obs_dimension, action_dimension, discrete, wandb_run=wandb_run,
                          device=device)
 
         self.logger = Logger(self.wandb_run, {
@@ -41,8 +41,8 @@ class PPO(Algorithm):
         self.value = ValueFunction(policy.input_size).to(self.device)
 
         self.replay_buffer = ReplayBuffer(self.hyperparameters.buffer_size, obs_dimension,
-                                          1 if is_discrete else action_dimension,
-                                          device=self.device, is_discrete=is_discrete)
+                                          1 if discrete else action_dimension,
+                                          device=self.device, discrete=discrete)
 
         self.policy_optimizer = torch.optim.Adam(self.policy.parameters(), lr=self.hyperparameters.lr, eps=1e-5)
         self.value_optimizer = torch.optim.Adam(self.value.parameters(), lr=self.hyperparameters.value_lr)
@@ -53,7 +53,7 @@ class PPO(Algorithm):
             action, log_probability, _ = self.policy.sample(obs)
         return action, log_probability
 
-    def update_and_observe(self, initial_obs, next_obs, action, action_log_prob : torch.Tensor, reward, done, timestep):
+    def update_and_observe(self, initial_obs, next_obs, action, action_log_prob : torch.Tensor, reward, done, timestep) -> bool:
         with torch.no_grad():
             reward = torch.tensor(reward, device=self.device, dtype=torch.float32)
             done = torch.tensor(done, device=self.device, dtype=torch.bool)
@@ -64,6 +64,8 @@ class PPO(Algorithm):
             self.gae_backwards()
             self.update_gradients(timestep)
             self.replay_buffer.reset()
+            return True
+        return False
 
     def td_error(self, initial_obs, next_obs, reward, terminal_mask):
         return reward + self.hyperparameters.gamma * terminal_mask * self.value(next_obs) - self.value(initial_obs)
@@ -75,6 +77,7 @@ class PPO(Algorithm):
             for k in range(self.replay_buffer.size-1, -1, -1):
                 obs, _, reward, _, next_obs, _, _, next_terminal = self.replay_buffer[k]
 
+                # the terminal mask is important so that we do not look past the episode boundary when doing gae
                 terminal_mask = ~next_terminal
 
                 td_error = self.td_error(obs, next_obs, reward, terminal_mask)
@@ -102,6 +105,7 @@ class PPO(Algorithm):
 
         value_criterion = torch.nn.MSELoss()
 
+
         for iteration in range(self.hyperparameters.gradient_epochs):
             for batch in dataloader:
                 obs, action, reward, old_policy_log_prob, next_obs, advantage, value_target, next_terminal = batch
@@ -118,20 +122,21 @@ class PPO(Algorithm):
 
                 self.policy_optimizer.step()
                 self.value_optimizer.step()
+                total_samples = len(dataloader) * self.hyperparameters.gradient_epochs * batch_length
 
                 with torch.no_grad():
                     entropy = distribution.entropy().sum()
                     batch_length = obs.size(0)
                     self.logger.add_log_data({
-                        "losses/policy_loss": policy_loss.item() * batch_length / len(dataloader),
-                        "losses/value_loss": value_loss.item() * batch_length/ len(dataloader),
-                        "losses/policy_entropy": entropy.item() / len(dataloader),
+                        "losses/policy_loss": policy_loss.item() * batch_length / total_samples,
+                        "losses/value_loss": value_loss.item() * batch_length/ total_samples,
+                        "losses/policy_entropy": entropy.item() / total_samples,
                     })
 
-            update_number = ((timestep // self.hyperparameters.buffer_size) - 1) * self.hyperparameters.gradient_epochs + iteration
+            # update_number = (timestep // self.hyperparameters.buffer_size) * self.hyperparameters.gradient_epochs + iteration
 
-            self.logger.set_log_data({'update_step': update_number})
-            self.logger.log_data()
-            self.logger.reset_fully()
+            # self.logger.set_log_data({'update_step': update_number})
+        self.logger.log_data()
+        self.logger.reset_fully()
 
 

src/algorithms/replay_buffer.py
@@ -8,12 +8,12 @@ class ReplayBuffer(Dataset):
 
     def __init__(self, capacity : int, obs_size : int, action_size : int, 
                  device : torch.device = torch.device("cpu"),
-                 is_discrete : bool = False):
+                 discrete : bool = False):
         # Convert your data into PyTorch tensors if they aren't already
         self.observations = torch.empty((capacity, obs_size), dtype=torch.float32, device=device)
         self.next_observations = torch.empty((capacity, obs_size), dtype=torch.float32, device=device)
-        if is_discrete:
-            self.actions = torch.empty(capacity, dtype=torch.int64, device=device)
+        if discrete:
+            self.actions = torch.empty((capacity, action_size), dtype=torch.int64, device=device)
         else:
             self.actions = torch.empty((capacity, action_size), dtype=torch.float32, device=device)
 

src/train.py
@@ -9,26 +9,41 @@ from src.algorithms.policies.categorical_policy import CategoricalPolicy
 from src.algorithms.policies.single_gaussian_policy import SingleGaussianPolicy
 from src.algorithms.ppo import PPOHyperparams
 from src.mdp.mdp_gym import MdpGym
+import os
+
 
 class Trainer:
 
-    def __init__(self, environment_id, algorithm_id : str, policy_id : str, hyperparameters : Hyperparameters, logging=True):
+    def __init__(self, environment_id, algorithm_id : str, policy_id : str, hyperparameters : Hyperparameters, logging=True, save_policy=False):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+        self.save_policy = save_policy
+        self.hyperparameters = hyperparameters
+
+        self.environment_id = environment_id
         now = datetime.now()
+        self.run_name = f"{self.environment_id}@{now:%Y-%m-%d-%H-%M-%S}"
         self.wandb_run = wandb.init(
             # Set the wandb entity where your project will be logged (generally your team name).
             entity="kieranparanjpe-mcgill-university",
             # Set the wandb project where this run will be logged.
             project="RL_Project1",
-            name=f"{environment_id}@{now:%Y-%m-%d-%H-%M-%S}",
+            name=self.run_name,
             tags=[f"{algorithm_id}", f"{policy_id}", f"{environment_id}"],
             job_type="train",
             # Track hyperparameters and run metadata.
-            config=hyperparameters.__dict__,
+            config=self.hyperparameters.__dict__,
         ) if logging else None
 
-        self.hyperparameters = hyperparameters
+        self.logger = Logger(self.wandb_run, {
+            "charts/episodic_return": 0.0,
+            "charts/episode_length": 0,
+            "global_step": 0,
+        })
+
+        if self.save_policy:
+            self.save_policy_folder = self.create_policy_folder()
+
         self.mdp = MdpGym(environment_id, self.device, render_mode=None)
 
         if policy_id == 'single_gaussian':
@@ -38,39 +53,45 @@ class Trainer:
 
         if algorithm_id == 'ppo':
             self.algorithm = PPO(self.hyperparameters, self.policy, self.mdp.obs_dimension,
-                                 self.mdp.action_dimension, self.mdp.is_discrete,
+                                 self.mdp.action_dimension, self.mdp.discrete,
                                  wandb_run=self.wandb_run, device=self.device)
 
+    def create_policy_folder(self):
+        directory_path = f"saved_policies/{self.environment_id}/{self.run_name}"
+
+        os.makedirs(directory_path, exist_ok=True)
+        return directory_path
+
+    def _save_policy(self, timestep):
+        torch.save(self.policy.state_dict(), f'{self.save_policy_folder}/policy_{timestep}.pth')
 
     def train(self):
-        log_data = Logger(self.wandb_run, {
-            "charts/episodic_return": 0.0,
-            "charts/episode_length": 0,
-            "global_step": 0,
-        })
-
         last_observation = self.mdp.reset()
         for timestep in tqdm(range(self.hyperparameters.n_timesteps)):
             action, log_prob_action = self.algorithm.sample_action(last_observation)
 
             next_observation, reward, done = self.mdp.step(action)
 
-            self.algorithm.update_and_observe(last_observation, next_observation, action, log_prob_action, reward,
+            updated_policy = self.algorithm.update_and_observe(last_observation, next_observation, action, log_prob_action, reward,
                                               done, timestep)
 
-            log_data.add_log_data({
+            if updated_policy and self.save_policy:
+                self._save_policy(timestep)
+
+            self.logger.add_log_data({
                 "charts/episodic_return": reward,
                 "charts/episode_length": 1,
-                "global_step": 1,
             })
             if done:
-                log_data.log_data()
-                log_data.reset_fields("charts/episodic_return", "charts/episode_length")
+                self.logger.set_log_data({'global_step': timestep})
+
+                self.logger.log_data()
+                self.logger.reset_fields("charts/episodic_return", "charts/episode_length")
 
                 last_observation = self.mdp.reset()
             else:
                 last_observation = next_observation
 
 if __name__ == "__main__":
-    trainer = Trainer("CartPole-v1", "ppo", "categorical", PPOHyperparams(), logging=True)
+    trainer = Trainer("CartPole-v1", "ppo", "categorical", PPOHyperparams(), logging=True, save_policy=True)
     trainer.train()